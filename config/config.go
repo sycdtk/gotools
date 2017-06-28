e6eaa467081d9076dc699d76b2352b633a3d5009
wolffy <78306909@qq.com>
2017-06-28 11:02:14 +0800

    配置读取

--- config/config.go
+++ config/config.go
@@ -1,21 +1,99 @@
 package config
 
 import (
-	"tools/file"
-	"tools/logger"
+	"bufio"
+	"io"
+	"os"
+	"strings"
 )
 
-const (
-	//配置文件名称
-	configFile = "config.conf"
-)
+const middle = "========="
 
-//检查或初始化配置文件
-func init() {
+type Config struct {
+	confMap map[string]string
+	strcet  string
+}
 
-	if file.Exist(configFile) {
-		logger.Debug("config.conf存在，开始读取配置……")
-	} else {
-		logger.Info("创建配置文件config.conf！")
+func (c *Config) InitConfig(path string) {
+	c.confMap = make(map[string]string)
+
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
 	}
+	defer f.Close()
+
+	r := bufio.NewReader(f)
+	for {
+		b, _, err := r.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+
+		s := strings.TrimSpace(string(b))
+		//fmt.Println(s)
+		if strings.Index(s, "#") == 0 {
+			continue
+		}
+
+		n1 := strings.Index(s, "[")
+		n2 := strings.LastIndex(s, "]")
+		if n1 > -1 && n2 > -1 && n2 > n1+1 {
+			c.strcet = strings.TrimSpace(s[n1+1 : n2])
+			continue
+		}
+
+		if len(c.strcet) == 0 {
+			continue
+		}
+		index := strings.Index(s, "=")
+		if index < 0 {
+			continue
+		}
+
+		frist := strings.TrimSpace(s[:index])
+		if len(frist) == 0 {
+			continue
+		}
+		second := strings.TrimSpace(s[index+1:])
+
+		pos := strings.Index(second, "\t#")
+		if pos > -1 {
+			second = second[0:pos]
+		}
+
+		pos = strings.Index(second, " #")
+		if pos > -1 {
+			second = second[0:pos]
+		}
+
+		pos = strings.Index(second, "\t//")
+		if pos > -1 {
+			second = second[0:pos]
+		}
+
+		pos = strings.Index(second, " //")
+		if pos > -1 {
+			second = second[0:pos]
+		}
+
+		if len(second) == 0 {
+			continue
+		}
+
+		key := c.strcet + middle + frist
+		c.confMap[key] = strings.TrimSpace(second)
+	}
+}
+
+func (c Config) Read(node, key string) string {
+	key = node + middle + key
+	v, found := c.confMap[key]
+	if !found {
+		return ""
+	}
+	return v
 }
--- config/config_test.go
+++ config/config_test.go
@@ -5,5 +5,39 @@ import (
 )
 
 func TestRead(t *testing.T) {
-	t.Log("run")
+	conf := &Config{}
+	conf.InitConfig("./config.conf")
+	if "werqwecxv" != conf.Read("www", "aaa") {
+		t.Fatal("aaa=werqwecxv")
+	}
+
+	if "twer etrew%%&%*" != conf.Read("zzz", "vv") {
+		t.Fatal("vv=twer etrew%%&%*")
+	}
+
+	if "c:/go" != conf.Read("default", "path") {
+		t.Fatal("path= c:/go")
+	}
+
+	if "1.44" != conf.Read("default", "version") {
+		t.Fatal("version = 1.44")
+	}
+
+	if "wrong" != conf.Read("test", "something") {
+		t.Fatal("something  = wrong  #注释1")
+	}
+
+	if "666" != conf.Read("test", "num") {
+		t.Fatal("num =	666")
+	}
+
+	if "" != conf.Read("test", "fdfdfd") {
+		t.Fatal("#fdfdfd = fdfdfd    注释整行")
+	}
+
+	if "refer" != conf.Read("test", "refer") {
+		t.Fatal("refer= refer       //注释3")
+	}
+
+	t.Log("success!")
 }
